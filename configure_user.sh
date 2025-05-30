9dcb17ee680910de92eaffeb9dc1831c8b9285fb
alphafox02 <44436101+alphafox02@users.noreply.github.com>
2025-05-29 20:46:12 -0400

    Update configure_user.sh

--- configure_user.sh
+++ configure_user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USER="jetson"
-PASSWORD="jetson"
-HOST="jetson"
+USER="dragon"
+PASSWORD="dragon"
+HOST="wardragon"
 
 export ARK_JETSON_KERNEL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-sudo $ARK_JETSON_KERNEL_DIR/prebuilt/Linux_for_Tegra/tools/l4t_create_default_user.sh -u $USER -p $PASSWORD -n $HOST -a --accept-license
+sudo $ARK_JETSON_KERNEL_DIR/prebuilt/Linux_for_Tegra/tools/l4t_create_default_user.sh -u $USER -p $PASSWORD -n $HOST --accept-license
